cda5be991716f44345a8fe953a77ef83d63776ed
Madson <maddytec@gmail.com>
2019-07-06 08:53:41 -0300

    Adicionado paginação na listagem de lançamentos

--- src/app/funcionario/components/listagem/listagem.component.ts
+++ src/app/funcionario/components/listagem/listagem.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { LancamentoService } from './../../../shared';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Lancamento } from 'src/app/shared';
+import { MatTableDataSource, MatSnackBar, MatSort, MatPaginator } from '@angular/material';
 
 @Component({
   selector: 'app-listagem',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListagemComponent implements OnInit {
 
-  constructor() { }
+  dataSource: MatTableDataSource<Lancamento>;
+  colunas: string[] = ['data', 'tipo', 'localizacao'];
+
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(
+    private lancamentoService: LancamentoService,
+    private matSnackBar: MatSnackBar) { }
 
   ngOnInit() {
+    this.lancamentoService.listarTodosLancamentos()
+      .subscribe(
+        data => {
+          const lancamentos = data['data'] as Lancamento[];
+          this.dataSource = new MatTableDataSource<Lancamento>(lancamentos);
+          this.dataSource.sort = this.sort;
+          this.dataSource.paginator = this.paginator;
+        },
+        err => {
+          const msg = 'Erro obtendo lançamentos.';
+          this.matSnackBar.open(msg, 'Erro', { duration: 6000 });
+        }
+      );
   }
 
 }
--- src/app/shared/services/lancamento.service.ts
+++ src/app/shared/services/lancamento.service.ts
@@ -8,24 +8,24 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class LancamentoService {
 
-private readonly PATH = 'lancamentos';
-private readonly PATH_ULTIMO_LANC = '/funcionario/{funcionarioId}/ultimo';
-private readonly PATH_LANCAMENTOS = '/funcionario/{funcionarioId}';
-private readonly PATH_TODOS_LANC = '/funcionario/{funcionarioId}/todos';
+  private readonly PATH = 'lancamentos';
+  private readonly PATH_ULTIMO_LANC = '/funcionario/{funcionarioId}/ultimo';
+  private readonly PATH_LANCAMENTOS = '/funcionario/{funcionarioId}';
+  private readonly PATH_TODOS_LANC = '/funcionario/{funcionarioId}/todos';
 
 
   constructor(
     private http: HttpClient,
-    private httpUtil: HttpUtilService ) { }
+    private httpUtil: HttpUtilService) { }
 
-buscarUltimoTipoLancado(): Observable<any> {
-return this.http.get(
-  environment.baseApiUrl + this.PATH +
-  this.PATH_ULTIMO_LANC.replace(
-    '{funcionarioId}', this.httpUtil.obterIdUsuario()),
-    this.httpUtil.headers()
-  );
-}
+  buscarUltimoTipoLancado(): Observable<any> {
+    return this.http.get(
+      environment.baseApiUrl + this.PATH +
+      this.PATH_ULTIMO_LANC.replace(
+        '{funcionarioId}', this.httpUtil.obterIdUsuario()),
+      this.httpUtil.headers()
+    );
+  }
 
   cadastrar(lancamento: Lancamento): Observable<any> {
     return this.http.post(
@@ -34,4 +34,14 @@ return this.http.get(
       this.httpUtil.headers()
     );
   }
+
+  listarTodosLancamentos(): Observable<any> {
+    return this.http.get(
+      environment.baseApiUrl + this.PATH +
+      this.PATH_TODOS_LANC.replace(
+        '{funcionarioId}', this.httpUtil.obterIdUsuario()),
+      this.httpUtil.headers()
+    );
+  }
 }
+
